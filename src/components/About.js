4fc04fba8ff804704734532680aaafd98c585ad7
leela khadka <khadkaleela3@gmail.com>
2022-08-30 18:28:26 -0700

    Completed test

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,28 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function isEmpty(val) {
+  return (val === undefined || val == null || val.length <= 0) ? true : false;
+}
+
+function NonEmptyBio(props) {
+  if (!isEmpty(props.bio)) {
+    return <DisplayBio bio={props.bio} />;
+  }
+  return null;
+}
+
+function DisplayBio(props) {
+  return <p>{props.bio}</p>;
+}
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {<NonEmptyBio bio={props.bio} />}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {<Links github={props.github} linkedin={props.linkedin} />}
     </div>
   );
 }
